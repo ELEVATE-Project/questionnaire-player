2b454243dc2fb8b8d05d419a5b3e2df79ae60e79
sagar <sagarkoshti1990@gmila.com>
2026-01-08 19:26:02 +0530

    fix:add api config showSaveDraftButton

--- projects/questionnaire-player/src/lib/interfaces/questionnaire.type.ts
+++ projects/questionnaire-player/src/lib/interfaces/questionnaire.type.ts
@@ -241,6 +241,7 @@ export interface ApiConfiguration{
   enablePagination?: boolean; // If false, display all page groups as cards instead of paginating
   gridCount?: number; // If provided, use grid layout with specified number of columns
   labelInputHorizontalLayout?: boolean; // If true, display label and input side by side horizontally
+  showSaveDraftButton?: boolean; // If true, show the save draft button, otherwise hide it
 }
 
 
